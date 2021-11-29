c52ac8bd7233c3893353ad70d740035ae34f7387
Mikael <dev.kov.mtl@gmail.com>
2021-11-29 15:29:03 -0500

    Show Loading and next seven day

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,7 @@
-export const INITIAL_CITY = 'Paris'
+export const INITIAL_CITY = 'New York'
+export const INITIAL_CITY_GEO = {
+  lat: 40.7143,
+  lon: -74.006,
+}
 
 export const APIID = 'da7f90c750a558dbf53529e0dff62a7d'
--- src/index.js
+++ src/index.js
@@ -1,26 +1,54 @@
 import './styles/index.css'
-import { APIID, INITIAL_CITY } from './constant'
-const loadingSpinnerContainer = document.querySelector(
-  '.loading-spinner-container'
-)
+import { APIID } from './constant'
+
+import { updateMainTempUI, updateDaysContainer } from './ui'
+const mainLoadingSpinner = document.querySelector('.main-loading-spinner')
 const mainContainer = document.querySelector('.main-container')
 const searchForm = document.querySelector('.search-form')
+const colWeekDayContainer = document.querySelector('.col-week-day-container')
 // const searchFormInput = document.querySelector('.search-form-input')
 // const searchFormSubmit = document.querySelector('.search-form-submit')
-const locationTitle = document.querySelector('.location-title')
-const locationDate = document.querySelector('.location-date')
-const locationTime = document.querySelector('.location-time')
-const locationTemperature = document.querySelector('.location-temperature')
-const changeTemperature = document.querySelector('.change-temperature')
-const feelLike = document.querySelector('.feel_like')
-const windSpeed = document.querySelector('.wind_speed')
-const moreHumidity = document.querySelector('.more_humidity')
-const moreInfoContainer = document.querySelector('.more-info-container')
-const cardMoreInfo = document.querySelector('.card-more-info')
-const colWeekDayContainer = document.querySelector('.col-week-day-container')
-const colDayCard = document.querySelector('.col-day-card')
 
-let lastCity = INITIAL_CITY
+// const changeTemperature = document.querySelector('.change-temperature')
+
+// const moreInfoContainer = document.querySelector('.more-info-container')
+// const cardMoreInfo = document.querySelector('.card-more-info')
+// const colDayCard = document.querySelector('.col-day-card')
+
+function geoSuccess(position) {
+  const { latitude, longitude } = position.coords
+  let lat = latitude
+  let lon = longitude
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minute&units=metric&appid=${APIID}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data)
+
+      const {
+        timezone,
+        current: { feels_like, humidity, wind_speed, temp, dt },
+      } = data
+
+      updateMainTempUI(timezone, '', dt, temp, feels_like, wind_speed, humidity)
+      updateDaysContainer(data.daily)
+
+      mainLoadingSpinner.classList.add('hidden')
+      mainContainer.classList.remove('hidden')
+    })
+    .catch((err) => {
+      // console.log(err)
+    })
+}
+
+function geoError() {
+  mainLoadingSpinner.classList.add('hidden')
+  mainContainer.classList.remove('hidden')
+  colWeekDayContainer.innerHTML = ``
+  alert('Sorry, no position available.')
+}
 
 function submitSearchForm(e) {
   e.preventDefault()
@@ -40,37 +68,41 @@ function submitSearchForm(e) {
         alert(data.message)
         return
       }
-      console.log(data)
+      // console.log(data)
       const {
         main: { temp, feels_like, humidity },
+        dt,
         wind: { speed },
         sys: { country },
         name,
         coord: { lat, lon },
       } = data
-      lastCity = `${name}`
-      locationTitle.textContent = `${name}, ${country}`
-      locationDate.textContent = `${new Date().toLocaleString().split(',')[0]}`
-      locationTime.textContent = `${new Date().toLocaleString().split(',')[1]}`
-      locationTemperature.textContent = `${temp} ℃`
-      feelLike.textContent = `${feels_like} ℃`
-      windSpeed.textContent = `${speed} km/h`
-      moreHumidity.textContent = `${humidity} %`
+
+      updateMainTempUI(name, country, dt, temp, feels_like, speed, humidity)
 
       fetch(
-        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minute&units=metric&appid=${APIID}`
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minute&units=metric&appid=${APIID}`
       )
         .then((response) => response.json())
         .then((data) => {
-          console.log(data)
+          // console.log(data)
+          updateDaysContainer(data.daily)
         })
-        .catch((err) => console.log(err))
+        .catch((err) => {
+          // console.log(err)
+        })
     })
     .catch((error) => {
-      console.log(error)
+      // console.log(error)
     })
 
   searchForm.reset()
 }
 
 searchForm.addEventListener('submit', submitSearchForm)
+
+// changeTemperature.addEventListener('click', (e) => {
+//   alert('Change F')
+// })
+
+navigator.geolocation.getCurrentPosition(geoSuccess, geoError)
